260fd9b2439211ddef7efe821188b059315896fd
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-07-14 14:55:12 +0530

    commit code for javascript

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-const todoList = [];
+let todoList = [];
+
+const addListToUl = (todoListItem) => {
+  const ul = document.querySelector("#todoList > ul");
+  const listItem = `<li id="${todoListItem.id}">
+    <input type="checkbox" ${
+      todoListItem.isDone ? "checked" : ""
+    } name="isDone" id="isDone" onchange="completeTodo(${todoListItem.id})" />
+    <span style="text-decoration: ${
+      todoListItem.isDone ? "line-through" : "none"
+    }">${todoListItem.todoText}</span>
+    <button type="button">Delete Todo</button>
+  </li>`;
+
+  ul.insertAdjacentHTML("afterbegin", listItem);
+};
+
+const clearUl = () => {
+  const ul = document.querySelector("#todoList > ul");
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
+};
 
 const todoForm = document.getElementById("todoForm");
 todoForm.onsubmit = (event) => {
@@ -13,15 +35,21 @@ todoForm.onsubmit = (event) => {
   todoList.push(todoListItem);
   todoText.value = "";
 
-  const ul = document.querySelector("#todoList > ul");
+  addListToUl(todoListItem);
+};
 
-  const listItem = `<li id="${todoListItem.id}">
-  <input type="checkbox" name="isDone" id="isDone" />
-  <span>${todoListItem.todoText}</span>
-  <button type="button">Delete Todo</button>
-</li>`;
-
-  ul.insertAdjacentHTML("afterbegin", listItem);
+const completeTodo = (id) => {
+  const index = todoList.findIndex((x) => x.id === id);
+  const updatedTodoList = [
+    ...todoList.slice(0, index),
+    { ...todoList[index], isDone: !todoList[index].isDone },
+    ...todoList.slice(index + 1),
+  ];
+  todoList = updatedTodoList;
+  clearUl();
+  for (let i = 0; i < updatedTodoList.length; i++) {
+    addListToUl(updatedTodoList[i]);
+  }
 };
 
 // document.addEventListener("mousemove", () => {
